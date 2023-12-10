0c58135f9ee8b966002969403f3f4d30465e2fa3
yinsumirage <2840369457@qq.com>
2023-12-10 19:44:06 +0800

    增设JS课程第二部分

---
+++ src/pages/js101/3Page.vue
@@ -0,0 +1,57 @@
+<template>
+  <q-page class="items-center fit q-pa-md">
+    <div class="tw-text-black">
+      <div
+        id="sandbox1"
+        class="mini-sandbox"
+        :class="$q.dark.isActive ? 'sandbox-theme-dark' : 'sandbox-theme-light'"
+      ></div>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  new MiniSandbox({
+    el: '#sandbox1',
+    files: {
+      'index.html': {
+        title: 'HTML',
+        defaultValue: `
+<!-- don't clear me -->
+<div style="margin-bottom:1rem;">Section 2:<br/>how JS run:</div>
+<div id ='example2'></div>
+        `.trim(),
+        jsLibs: ['index.js'],
+      },
+      'index.js': {
+        title: 'JS',
+        defaultValue: `
+// don't clear me
+const exa = document.querySelector('#example2');
+
+const myName = 'gwt'
+
+function name1(){
+  exa.innerHTML += myName;
+  exa.innerHTML += "<br/>";
+}
+
+function name2(){
+  exa.innerHTML += myName;
+  exa.innerHTML += "<br/>";
+}
+
+name1()
+name2()
+        `.trim(),
+      },
+    },
+    defaultConfig: {
+      height: '400px',
+    },
+  });
+});
+</script>
