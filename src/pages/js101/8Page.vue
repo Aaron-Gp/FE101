2b21c513e15e9e32fdd5a9de0be0e48b290e24aa
Song Yuyang <141478683+SUNNYsyy2005@users.noreply.github.com>
2023-12-17 10:44:25 +0800

    Update 8Page.vue

--- src/pages/js101/8Page.vue
+++ src/pages/js101/8Page.vue
@@ -1,12 +1,6 @@
 <template>
   <q-page class="items-center fit q-pa-md">
-    <div class="tw-text-black">
-      <div
-        id="sandbox1"
-        class="mini-sandbox"
-        :class="$q.dark.isActive ? 'sandbox-theme-dark' : 'sandbox-theme-light'"
-      ></div>
-    </div>
+    <div id="sandbox1"></div>
   </q-page>
 </template>
 
@@ -22,6 +16,8 @@ onMounted(() => {
         defaultValue: `
 <!-- don't clear me -->
 
+<div style="margin-bottom:1rem;">Console Output:</div>
+<div id="output"></div>
         `.trim(),
         jsLibs: ['index.js'],
       },
@@ -29,6 +25,22 @@ onMounted(() => {
         title: 'JS',
         defaultValue: `
 // don't clear me
+
+const output = document.querySelector('#output');
+
+output.innerHTML += "同步代码1<br/>";
+setTimeout(()=>{
+  output.innerHTML += "setTimeout<br/>";
+},0);
+new Promise((resolve)=>{
+  output.innerHTML += "同步代码2<br/>";
+  resolve();
+}).then(()=>{
+  output.innerHTML += "promise.then<br/>";
+}).then(()=>{
+  output.innerHTML += "promise.then111<br/>";
+})
+output.innerHTML += "同步代码3<br/>";
         `.trim(),
       },
     },
